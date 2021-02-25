24574eda7c9c9d5a59fe3d1d6843f13ffa3bd447
Björn Åström <b@strm.space>
2021-02-25 15:36:06 +0100

    Fix db shit

--- probecessor.py
+++ probecessor.py
@@ -26,20 +26,30 @@ if __name__ == "__main__":
         port = ip_row[1]
         print("Handling host {}:{}".format(ip, port))
 
-        for m in modules.modules.keys():
-            print("Examining {}".format(m))
-            c2 = dbh.cursor()
-            c2.execute("SELECT * FROM Probe WHERE ip = ? AND port = ? AND name = ?", (ip,port,m,))
 
-            rows = c2.fetchall()
+        c2 = dbh.cursor()
+        c2.execute("SELECT data FROM Probe WHERE name = 'port' AND type = 'open' AND ip = ? AND port = ?", (ip, port))
+        rows = c2.fetchall()
+        if len(rows) < 1:
+            print("No rows")
+            continue
+        row = rows[0]
+        m = row["data"].decode("utf-8")
+        if m == "unknown":
+            print("Unknown protocol")
+            continue
+        print("Examining {}".format(m))
+        c2 = dbh.cursor()
+        c2.execute("SELECT * FROM Probe WHERE ip = ? AND port = ? AND name = ?", (ip,port,m,))
+        rows = c2.fetchall()
 
-            if not rows or len(rows) == 0:
-                continue
+        if not rows or len(rows) == 0:
+            continue
 
-            mod = modules.modules.get(m)
-            if not mod:
-                print("Processor module '{}' does not exist".format(m))
-                continue
-            mod.run(rows)
+        mod = modules.modules.get(m)
+        if not mod:
+            print("Processor module '{}' does not exist".format(m))
+            continue
+        mod.run(rows)
 
     dbh.close()
