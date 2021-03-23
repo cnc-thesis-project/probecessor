34814e512dfdf4abe8409209b4fe4a3d8d1139bf
pedroingvar <0x696b7559@gmail.com>
2021-03-23 14:40:36 +0100

    return 'unlabeled' in Host.label_str() for unlabeled hosts

--- modules/host.py
+++ modules/host.py
@@ -38,6 +38,8 @@ class Host():
     def label_str(self, delimiter="/"):
         # convert labels into a string like: mirai/Dridex/QakBot
         label_set = set(map(lambda l: l.label, self.labels))
+        if len(label_set) == 0:
+            label_set.add("unlabeled")
         return delimiter.join(sorted(label_set))
 
 
