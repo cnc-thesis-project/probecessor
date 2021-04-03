9d4bd8d76756afb8602464ca086f10d3543994ef
pedroingvar <0x696b7559@gmail.com>
2021-04-03 17:56:13 +0200

    Move jarm initialization to load_fingerprints

--- methods/jarm.py
+++ methods/jarm.py
@@ -1,17 +1,11 @@
 import joblib
 import modules.label
 
-fp_hosts = {}
+jarm_label = {}
 
 def load_fingerprints(fp_path):
-    global fp_hosts
+    global jarm_label
     fp_hosts = joblib.load(fp_path)
-
-# TODO: 
-
-# Returns the fingerprint match. If none match, return None.
-def match(host, force=False):
-    jarms = dict()
     for fp_host in fp_hosts.values():
         for label in fp_host.labels:
             port = fp_host.ports.get(label.port)
@@ -19,26 +13,29 @@ def match(host, force=False):
                 continue
 
             jarm = port.tls.get_property("jarm")
-            if not jarm in jarms:
-                jarms[jarm] = {}
+            if not jarm in jarm_label:
+                jarm_label[jarm] = {}
 
-            if label.label in jarms[jarm]:
-                jarms[jarm][label.label] += 1
+            if label.label in jarm_label[jarm]:
+                jarm_label[jarm][label.label] += 1
             else:
-                jarms[jarm][label.label] = 1
+                jarm_label[jarm][label.label] = 1
 
-    for jarm in jarms:
+    for jarm in jarm_label:
         # get the most occuring label in each jarm hash
-        l = max(jarms[jarm].items(), key = lambda k: k[1])[0]
+        l = max(jarm_label[jarm].items(), key = lambda k: k[1])[0]
         # convert to Label object since match function needs to return in that format
-        jarms[jarm] = modules.label.Label("", l, None)
+        jarm_label[jarm] = modules.label.Label("", l, None)
 
+
+# Returns the fingerprint match. If none match, return None.
+def match(host, force=False):
     for port in host.ports.values():
         if not port or not port.tls:
             continue
         jarm = port.tls.get_property("jarm")
-        if jarm in jarms:
-            return (host, [jarms[jarm]])
+        if jarm in jarm_label:
+            return (host, [jarm_label[jarm]])
 
     #for port in host.ports.values():
     #    if port.tls:
