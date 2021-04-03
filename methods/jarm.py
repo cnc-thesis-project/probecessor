c3a4e75ca423c1da60eca551057c9926f38f6a09
pedroingvar <0x696b7559@gmail.com>
2021-04-03 21:11:15 +0200

    Avoid counting same port multiple times

--- methods/jarm.py
+++ methods/jarm.py
@@ -7,7 +7,12 @@ def load_fingerprints(fp_path):
     global jarm_label
     fp_hosts = joblib.load(fp_path)
     for fp_host in fp_hosts.values():
+        checked_ports = set()
         for label in fp_host.labels:
+            if label.port in checked_ports:
+                continue
+            checked_ports.add(label.port)
+
             port = fp_host.ports.get(label.port)
             if not port or not port.tls:
                 continue
@@ -36,12 +41,5 @@ def match(host, force=False):
         jarm = port.tls.get_property("jarm")
         if jarm in jarm_label:
             return (host, [jarm_label[jarm]])
-
-    #for port in host.ports.values():
-    #    if port.tls:
-
-    #for port in host.ports.values():
-    #    if port.tls:
-    #        print(port.tls.get_property("jarm"))
 
     return (host, [])
